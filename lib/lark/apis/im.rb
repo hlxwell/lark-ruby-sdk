d83676cdfb39bd2876bda9096517ce2147690bad
Michael He <michael.he@bytedance.com>
2021-04-06 21:33:57 +0900

    add get single chat info

--- lib/lark/apis/im.rb
+++ lib/lark/apis/im.rb
@@ -3,24 +3,31 @@ module Lark
     module Im
       # list all the chat rooms
       def chats(user_access_token:, page_size: nil, page_token: nil)
-        get 'im/v1/chats', {
+        get "im/v1/chats", {
           access_token: user_access_token,
           params: {
             page_size: page_size,
-            page_token: page_token
-          }
+            page_token: page_token,
+          },
+        }.compact
+      end
+
+      # get single chat room info
+      def chat(chat_id:, user_access_token:)
+        get "im/v1/chats/#{chat_id}", {
+          access_token: user_access_token,
         }.compact
       end
 
       # Search chat rooms of user is in
       def search(user_access_token:, query:, page_size: nil, page_token: nil)
-        get 'im/v1/chats/search', {
+        get "im/v1/chats/search", {
           access_token: user_access_token,
           params: {
             query: query,
             page_size: page_size,
-            page_token: page_token
-          }
+            page_token: page_token,
+          },
         }.compact
       end
 
